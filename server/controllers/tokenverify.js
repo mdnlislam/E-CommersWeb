552d7fd486a0aaa7061de885ac9eb84e7e05a34b
md.nurul <nurulislam8433@gmail.com>
2026-07-01 21:16:05 +0600

    auth

--- server/controllers/tokenverify.js
+++ server/controllers/tokenverify.js
@@ -18,7 +18,7 @@ const verifyToken = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findById(decoded._id).select("-password");
+    const user = await User.findById(decoded.id).select("-password");
 
     if (!user) {
       return res.status(401).json({
